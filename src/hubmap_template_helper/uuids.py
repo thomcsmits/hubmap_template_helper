e17821cfe4b5126db315fa15fcd78774041b8c04
thomcsmits <thomcsmits@outlook.com>
2023-11-14 06:32:45 -0500

    add get_all_uuids functionality

--- src/hubmap_template_helper/uuids.py
+++ src/hubmap_template_helper/uuids.py
@@ -1,3 +1,26 @@
+def get_all_uuids(metadata_url = 'https://portal.hubmapconsortium.org/metadata/v0/datasets.tsv'): 
+    '''
+    Retrieve a dictionary mapping uuids to hubmap ids.
+
+    Parameters
+    ----------
+    uuids : list
+        list with uuids
+    
+    Returns
+    -------
+    dictionary mapping uuids to hubmap ids
+    '''
+    # Fetch metadata, and read it into list
+    response = requests.get(metadata_url)
+    metadata = list(DictReader(StringIO(response.text), dialect=excel_tab))
+    
+    # The first item are the column headers, so remove these
+    all_uuids = [hit['uuid'] for hit in metadata[1:]]
+
+    return all_uuids
+
+
 def uuid_to_hubmap(uuids, metadata_url = 'https://portal.hubmapconsortium.org/metadata/v0/datasets.tsv'): 
     '''
     Retrieve a dictionary mapping uuids to hubmap ids.
