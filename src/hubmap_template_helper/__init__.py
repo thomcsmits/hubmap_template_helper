9a32a086d4c5e652540700cbcdb3b4e991137e2d
thomcsmits <thomcsmits@outlook.com>
2023-11-15 13:52:07 -0500

    add all functions to top level

--- src/hubmap_template_helper/__init__.py
+++ src/hubmap_template_helper/__init__.py
@@ -1,1 +1,4 @@
 from uuids import get_all_uuids, get_uuid_to_hubmap_mapping
+from compatibility import check_template_compatibility
+
+__all__ = ["get_all_uuids", "get_uuid_to_hubmap_mapping", "check_template_compatibility"]
