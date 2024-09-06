347047b71daa556644ef237b8a1e8f84baf2e0ba
thomcsmits <thomcsmits@outlook.com>
2024-09-06 08:44:58 -0400

    change dataset to assay_display_name in compatibility check

--- src/hubmap_template_helper/compatibility.py
+++ src/hubmap_template_helper/compatibility.py
@@ -4,13 +4,13 @@ import json
 import requests
 
 
-def check_template_compatibility(uuids, accepted_datatypes=None, required_filetypes=None,
+def check_template_compatibility(uuids, accepted_assay_display_names=None, required_filetypes=None,
                                  search_api='https://search.api.hubmapconsortium.org/v3/portal/search'):
     '''
     For a set of HuBMAP UUIDs, check if valid, and return valid UUIDs.
     Checks if UUIDs are present in the search API.
-    If accepted_datatypes is defined, checks if UUIDs are of any of
-        the datatypes in accepted_datatypes.
+    If accepted_assay_display_names is defined, checks if UUIDs are of any of
+        the assay_display_names in accepted_assay_display_names.
     If required_filetypes is defined, checks if UUIDs have all of the
         required filetypes in required_filetypes.
 
@@ -18,8 +18,8 @@ def check_template_compatibility(uuids, accepted_datatypes=None, required_filety
     ----------
     uuids : array of string
         HuBMAP UUIDs to be checked
-    accepted_datatypes: array of string, optional
-        accepted datatypes for template
+    accepted_assay_display_names: array of string, optional
+        accepted assay_display_names for template
     required_filetypes: array of string, optional
         required datatypes for template
     search_api: string, optional
@@ -34,22 +34,22 @@ def check_template_compatibility(uuids, accepted_datatypes=None, required_filety
         requests.post(
             search_api,
             json={
-                "size": 10000,
-                "query": {"ids": {"values": uuids}},
-                "_source": ["files", "data_types"]
+                'size': 10000,
+                'query': {'ids': {'values': uuids}},
+                '_source': ['files', 'assay_display_name']
             },
         ).text
-    )["hits"]["hits"]
+    )['hits']['hits']
 
     # create mapping for uuid to file_types and data_types
     uuid_to_files = {}
-    uuid_to_datatypes = {}
+    uuid_to_assay_display_names = {}
     for hit in hits:
         file_paths = [file['rel_path'] for file in hit['_source']['files']]
         uuid_to_files[hit['_id']] = file_paths
 
-        hit_data_type = hit['_source']['data_types']
-        uuid_to_datatypes[hit['_id']] = hit_data_type
+        hit_assay_display_name = hit['_source']['assay_display_name']
+        uuid_to_assay_display_names[hit['_id']] = hit_assay_display_name
 
     # save uuids without warnings
     accepted_uuids = uuids.copy()
@@ -58,8 +58,7 @@ def check_template_compatibility(uuids, accepted_datatypes=None, required_filety
     for uuid in uuids:
         # check if all uuids are found in the search api
         if uuid not in uuid_to_files.keys():
-            warnings.warn('Dataset with UUID "' + uuid +
-                          '" not found in Search API')
+            warnings.warn('Dataset with UUID "' + uuid + '" not found in Search API')
             accepted_uuids.remove(uuid)
             continue
 
@@ -73,13 +72,12 @@ def check_template_compatibility(uuids, accepted_datatypes=None, required_filety
                     if uuid in accepted_uuids:
                         accepted_uuids.remove(uuid)
 
-        if accepted_datatypes is not None:
+        if accepted_assay_display_names is not None:
             # check if data_types for each uuid are in accepted_datatypes
-            data_types = uuid_to_datatypes[uuid]
-            for data_type in data_types:
-                if data_type not in accepted_datatypes:
-                    warnings.warn('Dataset with UUID "' + uuid +
-                                  '" has unaccepted data type: ' + data_type)
+            assay_display_name = uuid_to_assay_display_names[uuid]
+            for assay in assay_display_name:
+                if assay not in accepted_assay_display_names:
+                    warnings.warn('Dataset with UUID "' + uuid + '" has unaccepted data type: ' + assay)
                     if uuid in accepted_uuids:
                         accepted_uuids.remove(uuid)
                     continue
